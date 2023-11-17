400b9b2ca4096aa5b5ddf52a7054b08b6ea038f1
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-11-17 03:56:23 -0300

    secondBranch

--- src/main.py
+++ src/main.py
@@ -11,6 +11,8 @@ from music21 import stream
 # Lista para guardar las notas extraídas
 notes = []
 
+def check_weights_exist(filepath):
+    return os.path.exists(filepath)
 
 def get_files(directory):
     global notes
@@ -39,28 +41,18 @@ def get_files(directory):
 
     return notes
 
-
-# Función para cargar las notas desde un archivo "notes" previamente guardado
 def load_notes(directory):
-    # Intentar abrir el archivo "notes"
     try:
         with open("notes", "rb") as filepath:
             notes = pickle.load(filepath)
-    # Si el archivo no existe, crearlo a partir del archivo MIDI
     except FileNotFoundError:
         notes = get_files(directory)
-    print('notesLoaded', notes)
     return notes
 
-
-# Obtener el número de notas diferentes en el archivo MIDI
 def prepare_sequences(notes):
-    # Crear un diccionario que mapea notas a enteros
     pitchnames = sorted(set(item for item in notes))
     note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
-    int_to_note = {number: note for note, number in note_to_int.items()}
 
-    # Crear la secuencia de entrada y salida
     sequence_length = 16
     network_input = []
     network_output = []
@@ -70,17 +62,14 @@ def prepare_sequences(notes):
         network_input.append([note_to_int[char] for char in sequence_in])
         network_output.append(note_to_int[sequence_out[0]])
 
-    # Convertir las secuencias de entrada y salida en arrays numpy y normalizar los valores
     n_patterns = len(network_input)
     network_input = np.reshape(network_input, (n_patterns, sequence_length, 1))
     network_input = network_input / float(len(pitchnames))
     network_output = to_categorical(np.array(network_output))
 
-    return network_input, network_output, int_to_note
+    return network_input, network_output, note_to_int, pitchnames
 
-
-def train(network_input, network_output, epochs):
-    # Definir la arquitectura de la red neuronal
+def train(network_input, network_output, note_to_int, pitchnames, epochs=2):
     model = Sequential()
     model.add(LSTM(128, input_shape=(network_input.shape[1], network_input.shape[2]), return_sequences=True))
     model.add(Dropout(0.3))
@@ -93,21 +82,23 @@ def train(network_input, network_output, epochs):
 
     model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-    # Definir el checkpoint para guardar los pesos de la mejor época
     filepath = "weights.best.hdf5"
     checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
 
-    # Entrenar la red neuronal y guardar los pesos de la mejor época
     model.fit(network_input, network_output, epochs=epochs, batch_size=350, callbacks=[checkpoint])
 
-    # Cargar los pesos de la mejor época
     model.load_weights(filepath)
+
+    # Guardar el mapeo note_to_int e int_to_note
+    with open('note_to_int.pickle', 'wb') as f:
+        pickle.dump(note_to_int, f)
+    int_to_note = {number: note for note, number in note_to_int.items()}
+    with open('int_to_note.pickle', 'wb') as f:
+        pickle.dump(int_to_note, f)
 
     return model
 
-
 def generate_music(model, network_input, int_to_note, n_vocab):
-    # Utilizar una semilla aleatoria de notas
     np.random.seed(42)
     sequence_length = 16
 
@@ -115,40 +106,33 @@ def generate_music(model, network_input, int_to_note, n_vocab):
     pattern = network_input[start]
     temperature = 1
 
-    # Generar 500 notas
     prediction_output = []
 
     for note_index in range(200):
         prediction_input = np.reshape(pattern, (1, len(pattern), 1))
         prediction_input = prediction_input / float(n_vocab)
 
-        # Predecir la siguiente nota utilizando el modelo
         prediction = model.predict(prediction_input, verbose=0)
 
-        # Ajustar la distribución de probabilidad con la temperatura
         prediction = np.log(prediction) / temperature
         exp_prediction = np.exp(prediction)
         prediction = exp_prediction / np.sum(exp_prediction)
 
-        # Muestrear la siguiente nota utilizando la distribución de probabilidad ajustada
         index = np.random.choice(range(n_vocab), size=1, p=prediction.flatten())[0]
         result = int_to_note[index]
         prediction_output.append(result)
 
-        # Agregar la nueva nota a la semilla y descartar la nota más antigua
         pattern = np.append(pattern, index)
         pattern = pattern[1:len(pattern)]
 
     return prediction_output
 
-
 def create_music(prediction_output):
     offset = 0
     output_notes = []
 
-    # Crear una nota o acorde para cada nota en la secuencia de salida
     for pattern in prediction_output:
-        if '.' in pattern:  # Si es un acorde
+        if '.' in pattern:
             notes_in_chord = pattern.split('.')
             notes = []
             for current_note in notes_in_chord:
@@ -163,7 +147,7 @@ def create_music(prediction_output):
             chord_note = chord.Chord(notes)
             chord_note.offset = offset
             output_notes.append(chord_note)
-        elif pattern.isdigit():  # Si es una sola nota
+        elif pattern.isdigit():
             new_note = note.Note()
             new_note.storedInstrument = instrument.Piano()
             try:
@@ -174,36 +158,43 @@ def create_music(prediction_output):
             except ValueError:
                 pass
 
-        # Avanzar la posición de la nota en el tiempo
         offset += np.random.choice([0.5, 1.0, 1.5, 2, 2.5, 3, 3.5, 4])
 
-    print('output notes', output_notes)
-    # Crear un objeto stream a partir de la lista de notas
     midi_stream = stream.Stream(output_notes)
-
-    # Exportar la secuencia de notas a un archivo MIDI
     midi_stream.write('midi', fp='output.mid')
 
-
+    
 if __name__ == '__main__':
-    # Especifica el directorio que contiene los archivos MIDI
     midi_directory = "C:/Users/southAtoms/Desktop/desarrollo/IAMidi/src/assets/midiFiles"
+    weights_file = "weights.best.hdf5"
 
-    # Cargar las notas del archivo "notes" (o crearlo a partir del archivo MIDI si no existe)
     notes = load_notes(midi_directory)
-
-    # Obtener el número de notas diferentes en el archivo MIDI
+    network_input, network_output, note_to_int, pitchnames = prepare_sequences(notes)
     n_vocab = len(set(notes))
 
-    # Preprocesar las notas y crear las secuencias de entrada y salida de la red neuronal
-    network_input, network_output, int_to_note = prepare_sequences(notes)
+    if check_weights_exist(weights_file):
+        model = Sequential()
+        model.add(LSTM(128, input_shape=(network_input.shape[1], network_input.shape[2]), return_sequences=True))
+        model.add(Dropout(0.3))
+        model.add(LSTM(128, return_sequences=True))
+        model.add(Dropout(0.3))
+        model.add(LSTM(128))
+        model.add(Dense(32))
+        model.add(Dropout(0.3))
+        model.add(Dense(len(pitchnames), activation='softmax'))
 
-    # Entrenar la red neuronal
-    epochs = 2  # Número de épocas que quieres entrenar
-    model = train(network_input, network_output, epochs)
+        model.load_weights(weights_file)
 
-    # Generar música utilizando el modelo
+        # Cargar el mapeo note_to_int e int_to_note
+        with open('note_to_int.pickle', 'rb') as f:
+            note_to_int = pickle.load(f)
+        with open('int_to_note.pickle', 'rb') as f:
+            int_to_note = pickle.load(f)
+    else:
+        model = train(network_input, network_output, note_to_int, pitchnames)
+        # Aquí también deberías cargar int_to_note después de entrenar
+        with open('int_to_note.pickle', 'rb') as f:
+            int_to_note = pickle.load(f)
+
     prediction_output = generate_music(model, network_input, int_to_note, n_vocab)
-
-    # Crear música y exportarla a un archivo MIDI
     create_music(prediction_output)
